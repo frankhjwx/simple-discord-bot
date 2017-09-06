1ad1ce3b859841d690ca8fa33f43e15133d4de5c
frankhjwx <frankhjwx@gmail.com>
2017-09-06 20:46:13 +0800

    Add files via upload

--- dicebot/bot.js
+++ dicebot/bot.js
@@ -66,42 +66,81 @@ client.on('message', message => {
 			case 'summon': 
 				var msg = "";
 				var files = [];
-				var num_3 = 10, num_equip = 10;
-				while (num_3 == 10 || num_equip == 10) {
-					files.length = 0;   // clear the array
+				var num_3 = 12, num_equip = 12;
+				while (num_3 == 12 || num_equip == 12) {
+					files = [];  // clear the array
 					num_3 = 0; num_equip = 0;
-					for (var i=0;i<10;i++) {
+					msg = "";
+					for (var i=0;i<12;i++) {
 						var Result = GetRandomNum(1,100);
-						if (Result < 1) {
+						if (Result < 2) {
 							var id = GetRandomNum(1,54);
 							files.push("img/servent/5/"+id+".jpg");
+							msg+="5☆s "+id+"   ";
 						} else if (Result < 5) {
 							var id = GetRandomNum(1,19);
 							files.push("img/equipments/5/"+id+".jpg");
 							num_equip++;
+							msg+="5☆e "+id+"   ";
 						} else if (Result < 9) {
 							var id = GetRandomNum(1,53);
 							files.push("img/servent/4/"+id+".jpg");
+							msg+="4☆s "+id+"   ";
 						} else if (Result < 25) {
 							var id = GetRandomNum(1,28);
 							files.push("img/equipments/4/"+id+".jpg");
 							num_equip++;
+							msg+="4☆e "+id+"   ";
 						} else if (Result < 39) {
 							var id = GetRandomNum(1,36);
 							files.push("img/servent/3/"+id+".jpg");
 							num_3++;
+							msg+="3☆s "+id+"   ";
 						}
 						else {
 							var id = GetRandomNum(1,46);
 							files.push("img/equipments/3/"+id+".jpg");
 							num_equip++;
 							num_3++;
+							msg+="3☆e "+id+"   ";
 						}
 					}
 				}
-				msg = msg + num_3 + " " + num_equip;
-				message.channel.send("",{files:[files[0],files[1],files[2],files[3],files[4],files[5],files[6],files[7],files[8],files[9]]});
+				var gm = require('gm');
+				
+				gm()
+					.in('-page','+0+0')
+					.in(files[0])
+					.in('-page','+128+0')
+					.in(files[1])
+					.in('-page','+256+0')
+					.in(files[2])
+					.in('-page','+384+0')
+					.in(files[3])
+					.in('-page','+0+140')
+					.in(files[4])
+					.in('-page','+128+140')
+					.in(files[5])
+					.in('-page','+256+140')
+					.in(files[6])
+					.in('-page','+384+140')
+					.in(files[7])
+					.in('-page','+0+280')
+					.in(files[8])
+					.in('-page','+128+280')
+					.in(files[9])
+					.in('-page','+256+280')
+					.in(files[10])
+					.in('-page','+384+280')
+					.in(files[11])
+					.mosaic()
+					.write('tmp.jpg', function (err) {
+						if (err) console.log(err);
+					});
+				message.channel.send("",{files:["tmp.jpg"]});
+				//message.channel.send(msg,{files:[files[0],files[1],files[2],files[3],files[4],files[5],files[6],files[7],files[8],files[9]]});
 				break;
+			
 		}
 	}
 });
